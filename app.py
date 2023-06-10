5d8c43adc79f6f3cbac48f263aa66b5f46bb2996
rawilk <rwilk1@outlook.com>
2023-06-10 17:32:25 +0200

    dekoratory

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
 from flask import Flask
+from datetime import datetime
 
+# Wszystko co jest potrzebne do utworzenia aplikacji webowej w podstawowej konfiguracji
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/')     # Przekazywanie dodatkowych parametrów w dekoratorze
 def hello_world():  # put application's code here
     return 'Hello World!'
 
-
+# Jeżeli my uruchamiamy ten program z tego pliku należy uruchomić metodę run()
 if __name__ == '__main__':
     app.run()
 
-# Dekoratory w Python-ie -
-def plus_one(number):
-    return number + 1
+@app.route('/date')
+def current_date():
+    return f'Response time: {str(datetime.now())}'
 
-print(plus_one(1))
+# End Point - to URL, czyli ciąg znaków wskazujący na jakiś "zasób" który jest osadzony na stronie internetowej
+# End Point to adres do zasobow
 
-my_function = plus_one
-print(my_function(5))
+# Lokal host: http://127.0.0.1:5000 dostęp tylko z tego komputera
+# żeby aplikacja była dostępna na zewnatrz trzeba byłoby ja zainstalowac na jakims serwerze zewnetrznym
+# na Azurze na wirtualnej maszynie na przklad
 
-def plus_one(number):
-    return number + 1
+counter = int()
+
+@app.route('/counter')
+def current_counter():
+    global counter  # bez tego nie da sie incrementowac zmiennej
+    counter += 1
+    return f'Liczba wywołan to: {counter}'
 
 
-def call_function(function):
-    number = 5
-    return function(number)
 
-
-print(call_function(plus_one))
 
 
 
--- decorator.py
+++ decorator.py
@@ -2,12 +2,19 @@
 def uppercase_decorator(function):
     def wrap():
         text = function()
+        print(text.upper())
         return text.upper()
     return wrap()
 
+# def Hello_World():
+#     return "Hello World"
+#
+# decorator = uppercase_decorator(Hello_World)
+# print(decorator)
+
+@uppercase_decorator
 def Hello_World():
     return "Hello World"
 
-decorator = uppercase_decorator(Hello_World)
-print(decorator)
-
+# @decorator - czyli to nazwa do której została przypisana jakaś funkcja
+# i dekorator może mieć dodatkowe właściwości
